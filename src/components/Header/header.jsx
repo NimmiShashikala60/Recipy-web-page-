fb9eddbfdc32437afd02c8cf37aea681d8487595
NimmiShashikala60 <nimmishashikala385@gmail.com>
2023-06-21 11:25:32 +0530

    first commit

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class header extends Component {
+  render() {
+    return (
+      <div className=''>
+        <h1>hgf</h1>
+      </div>
+    )
+  }
+}
